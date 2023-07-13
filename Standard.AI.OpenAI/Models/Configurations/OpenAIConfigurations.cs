4a6ccf0293a7923a0cfd57fa1f4ef298d6307aa0
sirasim <90763160+sirasim@users.noreply.github.com>
2023-07-13 19:50:20 +0500

    Add files via upload

---
+++ Standard.AI.OpenAI/Models/Configurations/OpenAIConfigurations.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standard.AI.OpenAI.Models.Configurations
+{
+    public class OpenAIConfigurations
+    {
+        public string ApiUrl { get; set; } = "https://api.openai.com/v1";
+        public string ApiKey { get; set; }
+        public string OrganizationId { get; set; }
+    }
+}
