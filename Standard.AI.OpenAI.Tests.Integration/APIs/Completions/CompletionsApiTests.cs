b6d03564f284ef99bdbf2e6a606ad5d72adbcb55
sirasim <90763160+sirasim@users.noreply.github.com>
2023-07-13 18:50:18 +0500

    Add files via upload

---
+++ Standard.AI.OpenAI.Tests.Integration/APIs/Completions/CompletionsApiTests.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+
+namespace Standard.AI.OpenAI.Tests.Integration.APIs.Completions
+{
+    public partial class CompletionsApiTests
+    {
+        private readonly IOpenAIClient openAIClient;
+
+        public CompletionsApiTests()
+        {
+            var openAIConfigurations = new OpenAIConfigurations
+            {
+                ApiKey = Environment.GetEnvironmentVariable("ApiKey"),
+                OrganizationId = Environment.GetEnvironmentVariable("OrgId"),
+                ApiUrl = "https://api.openai.com/"
+            };
+
+            this.openAIClient = new OpenAIClient(openAIConfigurations);
+        }
+    }
+}
